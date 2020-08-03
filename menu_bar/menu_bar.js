37ef820af88b22efcf132abee0ae2a6f8cecb97c
sudip ghimire <sudip@localhost.localdomain>
2020-08-03 23:04:04 +0545

    Added menu_bar

---
+++ menu_bar/menu_bar.js
@@ -0,0 +1,30 @@
+function toggle_menu(toggler){
+	let prev = toggler.previousElementSibling;
+	prev.classList.contains('show') ? 
+		prev.classList.remove('show') :
+		prev.classList.add('show');
+	toggler.textContent = (toggler.textContent == 'x') ? '=' : 'x';
+}
+
+
+let hided_on;
+function resized(){
+	let menubar = document.getElementById('MenuBar');
+	console.log(menubar.offsetWidth);
+	if(menubar.classList.contains('hide')){
+		if(menubar.offsetWidth > hided_on){
+			menubar.classList.remove('hide');
+		}
+	}
+	/* We are not sure if removing .hide is safe so check it again
+	   This is the case where very initial width was small and the again resized to smaller width
+	*/
+	let space = document.getElementsByClassName('navigation')[0].offsetLeft
+				-document.getElementsByClassName('logo')[0].offsetLeft;
+	if(space < 100){
+		if(!menubar.classList.contains('hide')){
+			hided_on = menubar.offsetWidth;
+			menubar.classList.add('hide');
+		}
+	}
+}
